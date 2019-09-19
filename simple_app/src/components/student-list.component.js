819a532d53fb201095859cc4ea7bc273ee0af03c
Nayana Laxmeshwar <nlaxmeshwar@mail.sfsu.edu>
2019-09-19 16:03:38 -0700

    added search component

--- simple_app/src/components/student-list.component.js
+++ simple_app/src/components/student-list.component.js
@@ -22,7 +22,7 @@ export default class StudentList extends Component{
         super(props);
 
         this.removeStudent = this.removeStudent.bind(this);
-        this.state = {students:[]};
+        this.state = {students:[],searchField:'',filteredStudents : []};
         // this.filteredStudents = [];
     }
 
@@ -45,25 +45,22 @@ export default class StudentList extends Component{
         })
     }
 
-    studentList() {
-        return this.state.students.map(currentstudent => {
-          return <Student student={currentstudent} removeStudent={this.removeStudent} key={currentstudent._id}/>;
-        })
-      }
 
     render(){
-      // this.filteredStudents = this.state.students.filter( student =>
-      //   student.first_name.toLowerCase().includes(searchField.toLowerCase())
-      //   );
-          // this.filteredStudents  =  this.state.students.filter( student =>
-          // student.first_name.toLowerCase().includes(this.state.searchField.toLowerCase())
-          // )
-          // this.setState({ students : this.filteredStudents })
-        // console.log(this.filteredStudents);
+      const filteredStudents = this.state.students.filter( student =>
+        student.first_name.toLowerCase().includes(this.state.searchField.toLowerCase()) ||
+        student.last_name.toLowerCase().includes(this.state.searchField.toLowerCase())
+        );
 
         return(
             <div>
         <h3>Student Information</h3>
+
+        <input type='search' 
+        placeholder='Search Students' 
+        onChange = {e=> this.setState({searchField:e.target.value})}/>
+        <p></p>
+        
         <table className="table">
           <thead className="thead-light">
             <tr>
@@ -77,7 +74,9 @@ export default class StudentList extends Component{
             </tr>
           </thead>
           <tbody>
-            { this.studentList() }
+          { filteredStudents.map(currentstudent => {
+          return <Student student={currentstudent} removeStudent={this.removeStudent} key={currentstudent._id}/>;
+        }) }
           </tbody>
         </table>
       </div>
